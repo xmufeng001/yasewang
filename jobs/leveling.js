916d45e69fc29a0f671ee417ed0972847590674b
wind wu <xmufeng001@gmail.com>
2013-08-06 00:45:18 +0800

    Revert "111"
    This reverts commit f24a8095db40cace3fd0966f0a16d4ecce5bc549.

--- jobs/leveling.js
+++ jobs/leveling.js
@@ -250,7 +250,7 @@ var battleLv4User=function(cookieString,user_id){
 }
 
 var goMap=function(cookieString){
-    var request_content_text = fs.readFileSync("./../map_request/tuesday/1.txt", "utf8");
+    var request_content_text = fs.readFileSync("./../map_request/monday/1.txt", "utf8");
     var headers=http_content_util.getHeaders(request_content_text);
     delete headers['Content-Length'];
     headers["Cookie"]=cookieString;
